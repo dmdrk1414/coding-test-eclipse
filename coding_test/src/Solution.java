e289e5a8393ed86d8c8833e15d9c1a2318285b46
dmdrk1414 <seungchan141414@gmail.com>
2024-06-17 22:48:41 +0900

    add

---
+++ coding_test/src/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = new int[] {1, 2, 3};
+
+		Print.print("sdf", arr);
+		Print.print("sdf", 'a');
+	}
+}
