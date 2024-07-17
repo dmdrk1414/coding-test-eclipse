02e971cc9678782a15d19aab3861a1f27f3c0a96
dmdrk1414 <seungchan141414@gmail.com>
2024-07-17 17:04:19 +0900

    aaaaaa

--- coding_test/src/Main.java
+++ coding_test/src/Main.java
@@ -2,120 +2,121 @@ import java.util.*;
 import java.io.*;
 
 /**
-7
-9
-1 2 4
-1 3 2
-1 4 3
-2 6 3
-2 7 5
-3 5 1
-4 6 4
-5 6 2
-6 7 5
-1 7
-
+ * 7 7
+ * #######
+ * #...RB#
+ * #.#####
+ * #.....#
+ * #####.#
+ * #O....#
+ * #######
+ * <p>
+ * 6 7
+ * #######
+ * #B....#
+ * #R....#
+ * #.....#
+ * #.....#
+ * #######
  */
 public class Main {
-	static Scanner sc = new Scanner(System.in);
-	static int N, M;
-	static List<Integer>[] adj;
-	static int[] ingree;
-	static int[][] map;
-	static StringBuilder sb;
-	static Queue<Integer> q;
-	static int[] maxValue;
-	static Node[] nodes;
-	static int start, end;
+    static int N, M, K;
+    static Scanner sc = new Scanner(System.in);
+    static long[] tree, arr;
 
-	private static void input() {
-		N = sc.nextInt();
-		M = sc.nextInt();
-		adj = new ArrayList[N + 1];
-		ingree = new int[N + 1];
-		map = new int[N + 1][N + 1];
-		for(int i = 0; i <= N ; i++) adj[i] = new ArrayList<>();
-		q = new LinkedList<>();
-		sb = new StringBuilder();
-		maxValue = new int[N + 1];
-		nodes = new Node[N + 1];
-		
-		
-		for(int i = 0; i < M ; i++) {
-			int one = sc.nextInt();
-			int two = sc.nextInt();
-			int value = sc.nextInt();
-			
-			map[one][two] = value;
-			adj[one].add(two);
-			ingree[two]++;
-		}
-		
-		start = sc.nextInt();
-		end = sc.nextInt();
+    private static void input() {
+        N = sc.nextInt();
+        M = sc.nextInt();
+        K = sc.nextInt();
+        arr = new long[N + 1];
+        tree = new long[N * 4];
 
-		for(int i = 1; i <= N ; i++) {
-			if(ingree[i] == 0) q.add(i);
-			nodes[i] = new Node(0, 0);
-		}
-		
-		// 시작 경로 카운트 ++
-		nodes[start].cnt++;
-		
-	}
 
-	private static void pro() {
-		bfs();
+        for (int i = 1; i <= N; i++) {
+            arr[i] = sc.nextLong();
+        }
 
-//		Out.print("확인", map);
-//		Out.print("확인", nodes);
-//		Out.print("확인", maxValue);
+        initTree(1, 1, N);
 
-		System.out.println(maxValue[end]);
-		System.out.println(nodes[end].cnt);
-	}
+        for (int i = 0; i < M + K; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
 
-	private static void bfs() {
-		while(!q.isEmpty()) {
-			int value = q.poll();
-			// ?
-			
-			for(int child : adj[value]) {
-				// 최대 거리 시간 들록
-				maxValue[child] = Math.max(maxValue[child], maxValue[value] + map[value][child]);
-				nodes[child].cnt = Math.max(nodes[child].cnt, nodes[value].cnt + 1);
-				
-				if(--ingree[child] == 0) {
-					q.add(child);
-				}
-			}
-		}
-	}
+            if (a == 1) {
+                long d = sc.nextLong();
 
-	public static void main(String[] args) throws IOException {
-		input();
-		pro();
-	}
-	
-	static class Node{
-		int value, cnt;
-		
-		public Node(int value, int cnt) {
-			this.value = value;
-			this.cnt = cnt;
-		}
+                // a가 1인 경우 b번째 수부터 c번째 수에 d를 더하고
+                update(b, c, d, 1, 1, N);
+            } else if (a == 2) {
+                // a가 2인 경우에는 b(1 ≤ b ≤ N)번째 수부터 c(b ≤ c ≤ N)번째 수까지의 합을 구하여 출력하면 된다.
+                long result = sumRec(b, c, 1, 1, N);
+                System.out.println(result);
+            }
+        }
+    }
 
-		@Override
-		public String toString() {
-			return "[value=" + value + ", cnt=" + cnt + "]";
-		}
-	}
+    private static long update(final int left, final int right, final long diff, final int node, final int nodeLeft, final int nodeRight) {
+        if (right < nodeLeft || nodeRight < left)
+            return tree[node];
 
-	private static void print(String string, int[] in2) {
-		System.out.print(string + ": "); System.out.println(Arrays.toString(in2));
-	}
+        if (nodeLeft == nodeRight)
+            return tree[node] += diff;
 
-	private static void print(String string, List<Integer> in2) {
-		System.out.print(string + ": "); System.out.println(in2);
-	}
+        if (left <= nodeLeft && nodeRight <= right) {
+            lazyExist[node] = true;
+            arr[node] += diff;
+            return tree[node] = mergeBlock(newValue, nodeRight - nodeLeft + 1);
+        }
+
+        int mid = nodeLeft + (nodeRight - nodeLeft) / 2;
+        if (lazyExist[node]) {
+            lazyExist[node] = false;
+            pushDown(lazyValue[node], node * 2, nodeLeft, mid);
+            pushDown(lazyValue[node], node * 2 + 1, mid + 1, nodeRight);
+            lazyValue[node] = DEFAULT_VALUE;
+        }
+
+        int leftVal = updateRec(left, right, newValue, node * 2, nodeLeft, mid);
+        int rightVal = updateRec(left, right, newValue, node * 2 + 1, mid + 1, nodeRight);
+        return tree[node] = merge(leftVal, rightVal);
+    }
+
+    private static long sumRec(final int left, final int right, final int node, final int nodeLeft, final int nodeRight) {
+        if (nodeRight < left || right < nodeLeft) {
+            return 0;
+        }
+
+        if (left <= nodeLeft && nodeRight <= right) {
+            return tree[node];
+        }
+
+        int mid = nodeLeft + (nodeRight - nodeLeft) / 2;
+        long leftVal = sumRec(left, right, node * 2, nodeLeft, mid);
+        long rightVal = sumRec(left, right, node * 2 + 1, mid + 1, nodeRight);
+        // 값의 변화가 되면 안된다 그냥 더한값을 출력
+//        return tree[node] = leftVal + rightVal;
+        return  leftVal + rightVal;
+    }
+
+    private static long initTree(final int node, final int left, final int right) {
+        if (left == right) {
+            return tree[node] = arr[left];
+        }
+
+        int mid = left + (right - left) / 2;
+        long leftVal = initTree(node * 2, left, mid);
+        long rightVal = initTree(node * 2 + 1, mid + 1, right);
+
+        return tree[node] = leftVal + rightVal;
+    }
+
+    private static void pro() {
+
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
 }
